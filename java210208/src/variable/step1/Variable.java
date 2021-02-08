ea44994d1ef3bc51cb6e7a18610fa4985ad7a184
kutaehwan <lion0523@naver.com>
2021-02-08 16:35:02 +0900

    2차 수정하였습니다. 작성자 : 구태환

---
+++ java210208/src/variable/step1/Variable.java
@@ -0,0 +1,25 @@
+package variable.step1;
+
+public class Variable {
+	// 선언부
+	
+	// 메소드 선언부
+	
+	// 메인 메소드 - exe파일로 만들 수 있다.
+
+	/********************************************
+	 * 문제해결능력 키움
+	 * @return void이다. - 비어있다. - 돌력 받을 수 있는 값이 없다.
+	 * - 이것때문에 에러가 발생 할 수도 있다.
+	 * @param args
+	 *******************************************/
+	public static void main(String[] args) {
+		// System : 너의 컴터 out속성 : 출력장치(하드웨어 - 모니터, 프린터...)
+		// System.out.print();		// 줄바꾼 안됨 , 에러
+		System.out.print(5);	// 목적이 있어 줄바꿈이 된다.
+		//print 메소드는 여러 개이다.
+		System.out.println();	// 줄바꾼 추가된 메소드
+
+	}
+
+}
