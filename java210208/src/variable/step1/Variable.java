52f83de3f9d4d8facd7d15dbf846d2d1010909dc
kutaehwan <lion0523@naver.com>
2021-02-08 17:32:41 +0900

    변수 선언 및 활용 예제

--- java210208/src/variable/step1/Variable.java
+++ java210208/src/variable/step1/Variable.java
@@ -4,7 +4,14 @@ public class Variable {
 	// 선언부
 	
 	// 메소드 선언부
+	void methodA(int x) {		
+		System.out.println(x);
+	}
 	
+	double methodB(int x) {
+		
+		return 3.14;
+	}
 	// 메인 메소드 - exe파일로 만들 수 있다.
 
 	/********************************************
@@ -15,10 +22,23 @@ public class Variable {
 	 *******************************************/
 	public static void main(String[] args) {
 		// System : 너의 컴터 out속성 : 출력장치(하드웨어 - 모니터, 프린터...)
-		// System.out.print();		// 줄바꾼 안됨 , 에러
-		System.out.print(5);	// 목적이 있어 줄바꿈이 된다.
+		// System.out.print();		// 반환값이 없다. , 에러
+		// System.out.print(); 		// 줄바꿈이 없다.
+		System.out.print(5);	// 메소드 호출
+		// 메소드 선언할 때는 좌중괄호 우중활고 를 사용하고....
+		// 메소드 호출할 때는 ;
+		// APIm XXX,jar, 이 안(클래스안에 객체 안에)에 있는 것을 꺼낸다.
 		//print 메소드는 여러 개이다.
-		System.out.println();	// 줄바꾼 추가된 메소드
+		// SELECT 이름 FROM 회원 집합
+		// WHERE 아이디 = 'test'
+		// AND 비번 = '123'
+		System.out.println();	// 줄바꿈 추가된 메소드
+		System.out.println(1+"   "+1);	// 줄바꿈 추가된 메소드
+		System.out.println(10+"10");	// 줄바꿈 추가된 메소드
+		
+		Variable v = new Variable();
+		System.out.println(v.methodB(1));
+		v.methodA(1);
 
 	}
 
